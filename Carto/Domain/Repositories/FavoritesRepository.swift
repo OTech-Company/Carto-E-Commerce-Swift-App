cb684603f588ae7b6410a325f34fefdf0c4b82b7
MennaMohamed23 <116288453+MennaMohamed23@users.noreply.github.com>
2026-07-03 15:17:21 -0700

    implement favorites repository protocol

---
+++ Carto/Domain/Repositories/FavoritesRepository.swift
@@ -0,0 +1,15 @@
+//
+//  FavoritesRepository.swift
+//  Carto
+//
+//  Created by Manona on 03/07/2026.
+//
+
+import Foundation
+
+protocol FavoritesRepository {
+    func getFavorites() -> [FavoriteItem]
+    func isFavorite(productId: Int) -> Bool
+    func addFavorite(_ item: FavoriteItem)
+    func removeFavorite(productId: Int)
+}
