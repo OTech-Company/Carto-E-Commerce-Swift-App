87fca59c8ee2a97fab5dc353a936ba64e6021da5
osama hosam abdellatif <osama.abdellatif.official@hotmail.com>
2026-07-11 14:41:41 +0300

    devevered code at the end of the hackathon merged with all mates

--- Packages/ARProductViewer/Sources/ARProductViewer/ARQuickLookPresenter.swift
+++ Packages/ARProductViewer/Sources/ARProductViewer/ARQuickLookPresenter.swift
@@ -14,7 +14,7 @@ public final class ARQuickLookPresenter: NSObject {
     private var modelURL: URL?
     private var onDismiss: (() -> Void)?
 
-    public static let shared = ARQuickLookPresenter()
+    @MainActor public static let shared = ARQuickLookPresenter()
     private override init() {}
 
     @MainActor public func present(modelURL: URL, from viewController: UIViewController, onDismiss: @escaping () -> Void = {}) {
